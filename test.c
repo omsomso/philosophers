bd74e44c8c084228e2619c6c8dbce78fb8ce7922
Krystian Pawlowski <kpawlows@c2r6s1.42lausanne.ch>
2023-02-13 16:07:28 +0100

    update

--- test.c
+++ test.c
@@ -8,8 +8,9 @@
 
 typedef struct	s_data
 {
-	pthread_mutex_t	lock[4];
+	pthread_mutex_t	lock[100]; //n + 1 for the general mutex
 	int				nb_phil;
+	int				eating_phil[100];
 	int				time_sleep;
 	int				time_eat;
 	int				time_death;
@@ -22,7 +23,7 @@ void	take_fourchette(t_data *data, int thread_id)
 	int	lckd_lo;
 
 	i = thread_id;
-	
+
 	lckd_hi = i + 1;
 	if (lckd_hi == data->nb_phil)
 		lckd_hi = 0;
@@ -30,20 +31,30 @@ void	take_fourchette(t_data *data, int thread_id)
 	if (lckd_lo < 0)
 		lckd_lo = data->nb_phil -1;
 	
-
-	printf("thread %d tries to access mutex idx %d, %d, %d\n", i, i, lckd_hi, lckd_lo);
-
+	pthread_mutex_lock(&data->lock[data->nb_phil]); //locks the locking but stalls if unlucky
+	printf("thread %d locked locking\n", thread_id);
+	//locks
 	pthread_mutex_lock(&data->lock[i]);
 	pthread_mutex_lock(&data->lock[lckd_hi]);
 	pthread_mutex_lock(&data->lock[lckd_lo]);
 
+	pthread_mutex_unlock(&data->lock[data->nb_phil]);
+	printf("thread %d unlocked locking\n", thread_id);
+
+
 	printf("thread %d locks mutex idxs %d, %d, %d, sleep...\n", thread_id, i, lckd_hi, lckd_lo);
 	sleep(1);
-	printf("thread %d unlocks mutex idxs %d, %d, %d, sleep...\n", thread_id, i, lckd_hi, lckd_lo);
+	printf("thread %d unlocks mutex idxs %d, %d, %d\n", thread_id, i, lckd_hi, lckd_lo);
 
 	pthread_mutex_unlock(&data->lock[i]);
 	pthread_mutex_unlock(&data->lock[lckd_hi]);
 	pthread_mutex_unlock(&data->lock[lckd_lo]);
+
+}
+int	timer2(int sec)
+{
+	sleep(sec);
+	return (1);
 }
 
 void	*thread_test(void *tmp)
@@ -55,7 +66,7 @@ void	*thread_test(void *tmp)
 	data =(t_data*)tmp;
 	thread_id = i;
 	i++;
-	take_fourchette(tmp, thread_id);
+	take_fourchette(data, thread_id);
 	printf("and thread %d ends now :(\n", thread_id);
 	return (NULL);
 }
@@ -63,30 +74,23 @@ void	*thread_test(void *tmp)
 int	main(void)
 {
 	int			i = -1;
-	int			n = 4;
+	int			n = 10;
 	pthread_t	thread[n];
 	t_data		data;
 
-	data.nb_phil = 0;
-	while (++i < n)
-	{
+	data.nb_phil = n;
+	while (++i <= n)
 		pthread_mutex_init(&data.lock[i], NULL);
-	//	usleep(0.1 * U_SEC);
-		data.nb_phil++;
-	}
-	/*printf("waiting...\n");
-	sleep(1);
-	printf("over\n");*/
 	i = -1;
 	while (++i < n)
 		pthread_create(&thread[i], NULL, thread_test, &data);
 	//pthread_create(&thread[0], NULL, thread_test, &data);
-	sleep(n * 5);
+	sleep(n + 2);
 	printf("program ends now\n");
 	//pthread_join(thread[0], NULL);
 	//pthread_join(thread[1], NULL);
 	i = -1;
-	while (++i < n)
+	while (++i <= n)
 		pthread_mutex_destroy(&data.lock[i]);
 	return (0);
 }
