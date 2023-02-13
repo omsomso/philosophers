c9e80efab167fbd1bac9b8179ee50416a4920c33
Krystian Pawlowski <kpawlows@c2r6s1.42lausanne.ch>
2023-02-13 12:14:40 +0100

    update

--- test.c
+++ test.c
@@ -6,43 +6,47 @@
 
 typedef struct	s_data
 {
-	pthread_mutex_t	lock_id;
+	pthread_mutex_t	lock[4];
+	//int	lock;
 } t_data;
 
 void	*thread_test(void *data)
 {
 	static int i = 0;
 
-	t_data	*dddd;
-	dddd =(t_data*)data;
-	pthread_mutex_lock(&dddd->lock_id);
+	t_data	*tmp;
+	tmp =(t_data*)data;
+	pthread_mutex_lock(&tmp->lock[0]);
 	i++;
 	printf("waiting in thread %d...\n", i);
 	sleep(1);
 	printf("and thread %d ends now :(\n", i);
-	pthread_mutex_unlock(&dddd->lock_id);
+	pthread_mutex_unlock(&tmp->lock[0]);
 	return (NULL);
 }
 
 int	main(void)
 {
-	pthread_t	thread_id[3];
-	int			i = 0;
+	int			i = -1;
+	int			n = 4;
+	pthread_t	thread[n];
 	t_data		data;
 
-	pthread_mutex_init(&data.lock_id, NULL);
-	printf("waiting...\n");
+	while (++i < n)
+		pthread_mutex_init(&data.lock[i], NULL);
+	/*printf("waiting...\n");
 	sleep(1);
-	printf("over\n");
-	while (i < 2)
-	{
-		pthread_create(&thread_id[i], NULL, thread_test, &data);
-		i++;
-	}
-	sleep(3);
+	printf("over\n");*/
+	i = -1;
+	while (++i < n)
+		pthread_create(&thread[i], NULL, thread_test, &data);
+	//pthread_create(&thread[0], NULL, thread_test, &data);
+	sleep(n);
 	printf("program ends now\n");
-	//pthread_join(thread_id[0], NULL);
-	//pthread_join(thread_id[1], NULL);
-	pthread_mutex_destroy(&data.lock_id);
+	//pthread_join(thread[0], NULL);
+	//pthread_join(thread[1], NULL);
+	i = -1;
+	while (++i < n)
+		pthread_mutex_destroy(&data.lock[i]);
 	return (0);
 }
