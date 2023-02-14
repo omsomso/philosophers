475f984494955dfd648279f51f9c31e5d6de7f90
omsomso <pkrystian.k@gmail.com>
2023-02-14 15:54:46 +0100

    death and max meals work

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kpawlows <kpawlows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 09:42:42 by kpawlows          #+#    #+#             */
-/*   Updated: 2023/02/14 13:41:02 by kpawlows         ###   ########.fr       */
+/*   Updated: 2023/02/14 15:54:01 by kpawlows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,58 @@ typedef struct	s_data
 {
 	pthread_mutex_t	lock;
 	int				nb_phil;
-	char			table_status[100]; //n
+	int				table_status[100]; //n
+	int				meals_had[100]; //n
 	long			death_hour[100]; //n
 	int				time_sleep;
 	int				time_eat;
 	int				time_death;
+	int				max_meals;
 } t_data;
+
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t'
+		|| c == '\v')
+		return (1);
+	return (0);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *s)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (s[i] && ft_isspace(s[i]) == 1)
+		i++;
+	if (s[i] == '+' && ft_isdigit(s[i + 1]) == 0)
+		return (0);
+	if (s[i] == '+')
+		i++;
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (s[i] && ft_isdigit(s[i]) == 1)
+	{
+		res *= 10;
+		res += (s[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
 
 void	arr_print(int *arr, int n)
 {
@@ -61,112 +107,124 @@ int		check_table_status(t_data *data, int id, int hi, int lo)
 void	find_lock_values(t_data *data, int thread_id, int *hi, int *lo)
 {
 	*hi = thread_id + 1;
-	if (*hi > data->nb_phil)
+	if (*hi >= data->nb_phil)
 		*hi = 0;
 	*lo = thread_id - 1;
-	if (*lo < 1)
+	if (*lo < 0)
 		*lo = data->nb_phil -1;
 }
 
-void	philosophise(t_data *data, int thread_id)
+int	philosophise(t_data *data, int thread_id, int hi, int lo)
 {
-	int	hi;
-	int	lo;
 	struct	timeval tv;
 	struct	timezone tz;
 
 	gettimeofday(&tv, &tz);
-
-	//check death timer here and detach if elapsed
-	//printf("time_death[%d] = %lu, current time = %lu\n", thread_id, data->death_hour[thread_id], tv.tv_sec);
-	if (data->death_hour[thread_id] < tv.tv_sec)
-	{
-		printf("philosopher %d died to death, simulation ends\n", thread_id);
-		exit(0); //that works??
-	}
-
 	find_lock_values(data, thread_id, &hi, &lo);
 	pthread_mutex_lock(&data->lock);
+	if (data->death_hour[thread_id] <= tv.tv_sec)
+		return (1);
 	if (check_table_status(data, thread_id, hi, lo) == 0)
 	{
 		set_table_status(data, thread_id, hi, lo, 1);
-		pthread_mutex_unlock(&data->lock); //let other threads lock while id sleeps
-		printf("philosopher %d eats...\n", thread_id); 
+		pthread_mutex_unlock(&data->lock); //let other philosophers check if forks are available
+		printf("philosopher %d eats...\n", thread_id);
+		if (++data->meals_had[thread_id] >= data->max_meals)
+			return (2);
 		usleep(data->time_eat * U_SEC); //philosopher eats
 		data->death_hour[thread_id] = tv.tv_sec + data->time_death; //death hour reset
 		set_table_status(data, thread_id, hi, lo, 0); //put the forks back on the table
 		printf("philosopher %d sleeps...\n", thread_id);
 		usleep(data->time_sleep * U_SEC); //start sleeping
-		printf("philosopher %d thinks...\n", thread_id); //thinking is waiting to eat woah
+		printf("philosopher %d thinks...\n", thread_id); //thinking is waiting to eat
 	}
 	else
-	{
 		pthread_mutex_unlock(&data->lock);
-		return ;
-	}
-}
-
-void	*timer2(void *tmp)
-{
-	t_data	*data;
-	data = (t_data*)tmp;
-	sleep(data->time_death); // sleep bc it is in a loop i think idl
-	return (NULL);
+	return (0);
 }
 
 void	*be_born(void *tmp)
 {
-	static int	i = 1;
+	static int	i = 0;
 	int			thread_id;
 	t_data		*data;
-	//pthread_t	thread_timer;
 	struct	timeval tv;
 	struct	timezone tz;
+	int		end_status;
 
 	data =(t_data*)tmp;
 	thread_id = i;
 	i++;
+	end_status = 0;
 	gettimeofday(&tv, &tz);
 	data->death_hour[thread_id] = tv.tv_sec + data->time_death;
-	printf("%ld\n", data->death_hour[thread_id]);
-	//pthread_create(&thread_timer, NULL, timer2, &data);
-	//if (thread_id == 3213)
-	while (1)
-		philosophise(data, thread_id);
+	while (end_status == 0)
+		end_status = philosophise(data, thread_id, 0, 0);
+	if (end_status == 1)
+		printf("philosopher %d died to death, simulation ends\n", thread_id);
+	if (end_status == 2)
+		printf("philosopher %d ate to immortality, simulation ends\n", thread_id);
+	exit (0); //not allowed...
 	return (NULL);
 }
 
-int	main(void)
+void	init_data(t_data *data, char **s)
+{
+	//number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
+	data->nb_phil = 10;
+	data->time_death = 20;
+	data->time_eat = 2;
+	data->time_sleep = 4;
+	data->max_meals = 5; 
+
+	/*data->nb_phil = ft_atoi(s[0]);
+	data->time_death = ft_atoi(s[1]);
+	data->time_eat = ft_atoi(s[2]);
+	data->time_sleep = ft_atoi(s[3]);
+	data->max_meals = ft_atoi(s[4]); //optional*/
+}
+
+int	check_input(char **s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != NULL)
+		i++;
+	//if (i < 5 || i > 6)
+	//	return (1);
+	//check if digit, max and min int
+	//porb not needed to check if death < sleep or smth
+	return (0);
+}
+
+int	main(int argc, char **argv)
 {
 	int			i = -1;
-	int			n = 10;
-	pthread_t	thread[n];
 	t_data		data;
 
-
-	//number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
-
-	data.nb_phil = n;
-	data.time_eat = 2;
-	data.time_sleep = 5;
-	data.time_death = 2;
+	if (check_input(++argv) != 0)
+		return (1);
+	init_data(&data, argv);
+	
+	pthread_t	thread[data.nb_phil];
+	
 	pthread_mutex_init(&data.lock, NULL);
-	memset(data.table_status, 0, n);
-	//memset(data.death_hour, 0, n);
-	while (++i < n)
+	memset(data.table_status, 0, data.nb_phil * sizeof(int));
+	memset(data.death_hour, 0, data.nb_phil * sizeof(int));
+	memset(data.meals_had, 0, data.nb_phil * sizeof(int));
+	while (++i < data.nb_phil)
 		data.table_status[i] = 0;
 	i = -1;
-	while (++i < n)
+	while (++i < data.nb_phil)
 		pthread_create(&thread[i], NULL, be_born, &data);
-	//pthread_create(&thread[0], NULL, thread_test, &data);
-	sleep(60);
+	sleep(360);
 	//arr_print(data.table_status, 10);
 	printf("program ends now\n");
 	//i = -1;
 	//while (++i < n)
 	//	pthread_join(thread[i], NULL);
 	//pthread_join(thread[1], NULL);
-	i = -1;
 	pthread_mutex_destroy(&data.lock);
 	return (0);
 }
